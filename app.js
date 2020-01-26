bdcf98a377cb36beb98c832acac0287870900d2b
nagma <bano.nagma.1@gmail.com>
2020-01-27 01:21:51 +0530

    product function

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const add = require('./utils.js')
 
-const sum = add(4,-3)
+const multiply = require('./operator')
 
+const sum = add(4,-3)
+const product = multiply(4,3)
 console.log(sum);
+console.log(product);
+
